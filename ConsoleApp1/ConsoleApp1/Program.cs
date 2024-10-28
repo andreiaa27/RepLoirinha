8127f508c2661ab6834cdb097e0f840e32890268
andreiaa27 <aasmachado987@gmail.com>
2024-10-28 17:40:28 +0000

    CA

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; 
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nHello World!");
+            Console.WriteLine();
+            Console.WriteLine("\nHello World 1!");
+            Console.WriteLine();
+            Console.WriteLine("\nHello World 2!");
+            Console.WriteLine();
+            Console.WriteLine("\nHello Daniel!");
+
+            Console.ReadKey();
+        }
+    }
+}
