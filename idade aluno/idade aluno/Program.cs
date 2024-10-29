bee76887513ee7dcdd59ded49d6cf523f02d2d1e
andreiaa27 <aasmachado987@gmail.com>
2024-10-29 15:41:24 +0000

    idade

---
+++ idade aluno/idade aluno/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace idade_aluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nome: ");
+           string nome1 = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            int idade1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nome: ");
+            string nome2 = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            int idade2 = int.Parse(Console.ReadLine());
+
+            if (idade1 < idade2)
+            {
+                Console.WriteLine(nome2 + " é mais velho");
+            }
+            else if (idade1 == idade2)
+            {
+                Console.WriteLine(nome1 + " e " + nome2 + " têm a mesma idade");
+            }
+            else
+            {
+                Console.WriteLine(nome1 + " é mais velho");
+            }
+            Console.ReadKey();
+            
+
+
+
+
+
+
+        }
+    }
+}
