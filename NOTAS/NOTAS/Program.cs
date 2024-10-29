3a77148262099ab965cc19b6c7e63373fc84f9b8
andreiaa27 <aasmachado987@gmail.com>
2024-10-29 15:15:51 +0000

    notas

---
+++ NOTAS/NOTAS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NOTAS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Nota:");
+           double nota = double.Parse(Console.ReadLine());
+
+            if (nota <= 5) {
+                Console.WriteLine("Muito Insuficiente");
+            }
+            else if (nota <= 9.5) {
+                Console.WriteLine("Insuficiente");
+            }
+            else if (nota > 9.5  && nota < 15) {
+                Console.WriteLine("Suficiente");
+            }
+            else
+            {
+                Console.WriteLine("Bom");
+            }
+            Console.ReadKey();
+        }
+    }
+}
