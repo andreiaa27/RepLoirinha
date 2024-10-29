10ce2345f47ee3c21517f5b00e516d2f55b893c7
andreiaa27 <aasmachado987@gmail.com>
2024-10-29 16:23:52 +0000

    salarios

---
+++ salarios/salarios/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace salarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nome: ");
+            string nome1 = Console.ReadLine();
+            Console.WriteLine("Salario: ");
+            int salario1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nome: ");
+            string nome2 = Console.ReadLine();
+            Console.WriteLine("Salario: ");
+            int salario2 = int.Parse(Console.ReadLine());
+
+            double salarios = (salario1 + salario2) / 2;
+            Console.WriteLine("A média dos salários é " + salarios);
+
+            Console.ReadKey();
+        }
+    }
+}
